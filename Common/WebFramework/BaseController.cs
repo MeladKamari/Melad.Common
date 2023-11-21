453e0184956d6539f81d031f0ba2fd8419e27719
Melad Hamedani <MeladHamedani@gmail.com>
2023-11-22 02:19:05 +0330

    :sparkles: Add BaseController

---
+++ Common/WebFramework/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Melad.Common.WebFramework;
+/// <summary>
+/// Base Controller
+/// </summary>
+[Route("api/v{version:apiVersion}/[controller]/[action]")]
+[ApiVersion("1")]
+[ApiController]
+[ApiResultFilter]
+public class BaseController : ControllerBase
+{
+    #region Fields
+    private readonly IMediator _mediator;
+    #endregion
+    #region Ctor
+    /// <summary>
+    /// Creates a BaseController
+    /// </summary>
+    protected BaseController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    #endregion
+}
